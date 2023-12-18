032dd439af79d8a4d8151cddf05d714e79b58b2b
yong <e2yong98@gmail.com>
2023-12-19 00:12:01 +0900

    HTTP API Message Body(text/plain)

--- 3 mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ 3 mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -1,9 +1,30 @@
 package hello.servlet.basic.request;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
 
-// HTTP 메시지 바디에 데이터를 직접 담아서 요청
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+// HTTP 메시지 바디에 데이터(text/plain)를 직접 담아서 요청
 @WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
 public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(
+                inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
 }
