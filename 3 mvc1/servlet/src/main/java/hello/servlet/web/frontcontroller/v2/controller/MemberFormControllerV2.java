b046608e014fad96f8715371c8fb963fe158f99a
yong <e2yong98@gmail.com>
2024-01-10 13:08:58 +0900

    MVC 변화

---
+++ 3 mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // V1
+//        String viewPath = "/WEB-INF/views/new-form.jsp";
+//        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+//        dispatcher.forward(request, response);
+
+        // V2
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
